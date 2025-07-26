e922b3553a624439660180d9dd5e64464173a855
Fidelis <fb46us@gmail.com>
2025-07-26 23:58:13 +0700

    init frontend (vite)

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,7 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" }); // Make sure this runs exactly once, at the top.
 
-console.log("Port:", process.env.PORT);
-
 export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY || "";
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.BACKEND_PORT || 3000;
